2e1c6ca9477e17a71ea948e22de148c23cb01835
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-11-14 23:37:01 +0200

    :

--- wildlife-edu/src/pages/About.js
+++ wildlife-edu/src/pages/About.js
@@ -1,8 +1,5 @@
-// src/pages/About.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { db } from './Firebase'; 
-import { collection, addDoc } from 'firebase/firestore'; 
 import './About.css';
 import './Contact.css';
 
@@ -27,32 +24,55 @@ const About = () => {
 
   return (
     <div className="Aboutus">
+      {/* Existing About Us content */}
       <div className="about about-fade-in-up">
-        <div>
-          <h1>About Us</h1>
-          <p className='p'>
-            Endangered Animals in Rwanda’s Akagera National Park is the particular focus of the study, and it will be undertaken in partnership with local communities, nature care organizations, as well as experts.
-          </p>
-        </div>
-        <img src="/elephants.jpg" alt="Mission" />
-        <div className="about-images">
-          <div className="about-image about-fade-in-left">
-            <h1>Our Mission</h1>
-            <p className='p'>We believe that education is the key to fostering a deeper understanding of wildlife.</p>
-            <img src="/zebra.jpg" alt="Mission" />
-          </div>
-          <div className="about-image about-fade-in-right">
-            <h1>Our Vision</h1>
-            <p className='p'>Join our community today and start your journey toward making a difference!</p>
-            <img src="/elephants.jpg" alt="Vision" />
-          </div>
-        </div>
-      </div> 
+        {/* About Us Section */}
+        {/* Mission and Vision */}
+      </div>
+
       {/* Call to Action Section */}
       <section className="call-to-action about-fade-in-up">
         <h2>Join Our Community</h2>
         <p>Engage with other wildlife enthusiasts and experts.</p>
         <Link to="/login" className="cta-button">Join the Forum</Link>
+      </section>
+
+      {/* Partnerships Section */}
+      <section className="partnerships about-fade-in-left">
+        <h2>Our Partnerships</h2>
+        <p>Collaborating with like-minded organizations to make a difference.</p>
+        <div className="partners">
+          <div className="partner">
+            <img src="/partner1.jpg" alt="Partner 1" />
+            <Link to="/blog/1" className="read-more">Diana Fosel</Link>
+          </div>
+          <div className="partner">
+            <img src="/partner2.jpg" alt="Partner 2" />
+            <Link to="/RDB" className="read-more">RDB</Link>
+          </div>
+          {/* Add more partner elements as needed */}
+        </div>
+      </section>
+
+      {/* Blogs Section */}
+      <section className="blogs about-fade-in-right">
+        <h2>Our Blog</h2>
+        <p>Stay updated with our latest posts and wildlife stories.</p>
+        <div className="blog-posts">
+          <div className="blog-post">
+            <img src="/blog1.jpg" alt="Blog Post 1" />
+            <h3>Blog Post Title 1</h3>
+            <p>Summary of the blog post goes here...</p>
+            <Link to="/blog/1" className="read-more">Read More</Link>
+          </div>
+          <div className="blog-post">
+            <img src="/blog2.jpg" alt="Blog Post 2" />
+            <h3>Blog Post Title 2</h3>
+            <p>Summary of the blog post goes here...</p>
+            <Link to="/blog/2" className="read-more">Read More</Link>
+          </div>
+          {/* Add more blog post elements as needed */}
+        </div>
       </section>
     </div>
   );
